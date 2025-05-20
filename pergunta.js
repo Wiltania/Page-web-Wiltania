a70dfb08556539e50fb3156d0c889263cfd0df18
Wiltania <wiltania.pierre-louis@escola.pr.gov.br>
2025-05-20 00:25:32 -0300

    Add files via upload
    realização das aulas 4, 5, 6 e 7 em casa.

---
+++ pergunta.js
@@ -0,0 +1,25 @@
+criarCartao(
+   "Física",
+   "O que é velocidade média?" ,
+   " É a razão entre a variação de espaço e o tempo gasto: v = Δs / Δt" 
+)
+criarCartao(
+   "Química",
+   "Qual é a fórmula da água?" ,
+   "  H₂O" 
+)
+criarCartao(
+   "Matemática",
+   " Qual é a fórmula da área de um triângulo?" ,
+   "  A = (base . altura) / 2" 
+)
+criarCartao(
+   "Francês",
+   " Qual o verbo “être” no presente do indicativo para 'nous'?" ,
+   "  Nous sommes." 
+)
+criarCartao(
+   "Literatura",
+   " O que é o 'eu lírico'?" ,
+   "  É a voz que fala no poema." 
+)
