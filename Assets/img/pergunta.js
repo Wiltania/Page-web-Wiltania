8936f7dcf584c6136d4294bb3c90f36dd4d2978f
Wiltania <wiltania.pierre-louis@escola.pr.gov.br>
2025-05-21 18:30:11 -0300

    PROJETO FINAL
    Retirada de fotos e adição do fundo

---
+++ Assets/img/pergunta.js
@@ -0,0 +1,62 @@
+criarCartao(
+   "Literatura",
+   "Quem é a autora da obra 'Quarto de despejo?" ,
+   "Carolina Maria de Jesus" 
+)
+criarCartao(
+   "Física",
+   "O que é velocidade média?" ,
+   " É a razão entre a variação de espaço e o tempo gasto: v = Δs / Δt" 
+)
+criarCartao(
+   "Curiosidades",
+   "Você sabia?" ,
+   " O coração de um camarão fica na cabeça." 
+)
+criarCartao(
+   "Matemática",
+   " Qual é a fórmula da área de um triângulo?" ,
+   "  A = (base . altura) / 2" 
+)
+criarCartao(
+   "Francês",
+   " Qual o verbo “être” no presente do indicativo para 'nous'?" ,
+   "  Nous sommes." 
+)
+criarCartao(
+   "Literatura",
+   " O que é o 'eu lírico'?" ,
+   "  É a voz que fala no poema." 
+)
+criarCartao(
+   "Física",
+"O que é força?",
+"É a grandeza que pode alterar o estado de movimento ou repouso de um corpo"
+)
+criarCartao(
+   "Química",
+   " O que são elementos químicos?" ,
+   "  São substâncias formadas por átomos do mesmo tipo." 
+)
+criarCartao(
+   "Matemática",
+   " O que é o 'eu lírico'?" ,
+   "  É a voz que fala no poema." 
+)
+criarCartao(
+   "Curiosidades",
+   "Você sabia?" ,
+   " Um caracol pode dormir por até 3 anos." 
+)
+
+criarCartao(
+   "Literatura",
+   "Quais são as principais características do Romantismo?" ,
+   "  Valorização da emoção, idealização do amor e da mulher, nacionalismo e subjetividade." 
+)
+criarCartao(
+   "Francês",
+   " Como se fala relógio de pulso?" ,
+   "  Montre." 
+)
+
